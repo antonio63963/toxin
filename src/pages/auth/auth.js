12dbbcff9db76e70a81f129e5fdc80dcd23a5b8c
antonio63963 <shotvideo25@gmail.com>
2024-04-14 23:18:21 +0300

    switch between login and signUp

--- src/pages/auth/auth.js
+++ src/pages/auth/auth.js
@@ -1,2 +1,22 @@
 import "../../ui/index.scss";
+import "../../ui/utils.scss";
 import './auth.scss';
+
+const loginForm = document.querySelector('.auth-loginForm');
+const signUpForm = document.querySelector('.auth-signUpForm');
+const toSignUpButton = document.querySelector('.loginForm__toSignUp');
+const toLoginButton = document.querySelector('.signUpForm__toLogin');
+
+function onToSignUp(e) {
+  e.preventDefault();
+  loginForm.classList.add('hide');
+  signUpForm.classList.remove('hide')
+}
+function onToLogin(e) {
+  e.preventDefault();
+  loginForm.classList.remove('hide');
+  signUpForm.classList.add('hide')
+}
+
+toSignUpButton.addEventListener('click', onToSignUp);
+toLoginButton.addEventListener('click', onToLogin);
