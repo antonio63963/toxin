93d61a0c78ed3fda6a478d63e1050c8e9cce36e0
antonio63963 <shotvideo25@gmail.com>
2024-04-28 14:09:34 +0300

    fixed bugs with guest select

--- src/ui/inputs/guestsSelect/guestsSelect.js
+++ src/ui/inputs/guestsSelect/guestsSelect.js
@@ -1,7 +1,8 @@
-const dropdown = document.querySelector('.sidebar-rooms__guests');
+const dropdown = document.querySelector('.sidebar-rooms__guests-input');
 const dropdownList= document.querySelector('.dropdown__list');
 
 dropdown.addEventListener('click', (e) => {
+  console.log(e.target)
   if(e.target.closest('.dropdown__list')) return;
   dropdown.classList.toggle('dropdown-open');
   dropdownList.classList.toggle('dropdown__list-hide');
