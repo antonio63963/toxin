f7753b2ac81fc2e28529fe8933c7aaaf36d29a39
antonio63963 <shotvideo25@gmail.com>
2024-05-09 16:56:06 +0300

    offerForm in process, top, date, guests

--- src/pages/roomDetails/roomDetails.js
+++ src/pages/roomDetails/roomDetails.js
@@ -4,3 +4,4 @@ import '../../ui/voiceChart/voiceChart.js';
 import './components/roomHeadline/roomHeadline.scss';
 import './components/roomInfo/roomInfo.scss';
 import './components/roomImpressions/roomImpressions.scss';
+import './components/offerForm/offerForm.scss';
