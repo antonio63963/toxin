0e468f9e3fa39593bcef8fca19ca7d3bf109b9b7
antonio63963 <shotvideo25@gmail.com>
2024-05-11 20:40:59 +0300

    review feedback block, start adaptive

--- src/pages/roomDetails/roomDetails.js
+++ src/pages/roomDetails/roomDetails.js
@@ -5,3 +5,4 @@ import './components/roomHeadline/roomHeadline.scss';
 import './components/roomInfo/roomInfo.scss';
 import './components/roomImpressions/roomImpressions.scss';
 import './components/offerForm/offerForm.scss';
+import './components/reviews/reviews.scss';
